d76f2e321c82d493f57c0ca525f8ee9693c374f9
prajwalshette <prajwalshette13@gmail.com>
2025-04-16 16:54:43 +0530

    comment role middleware

--- src/middlewares/role.middleware.ts
+++ src/middlewares/role.middleware.ts
@@ -1,22 +1,22 @@
-import { NextFunction, Response } from 'express';
-import { RequestWithUser } from '../interfaces/auth.interface';
-import { HttpException } from '../exceptions/HttpException';
-import { Role } from '@prisma/client';
+// import { NextFunction, Response } from 'express';
+// import { RequestWithUser } from '../interfaces/auth.interface';
+// import { HttpException } from '../exceptions/HttpException';
+// import { Role } from '@prisma/client';
 
-export class RoleMiddleware {
-  public verifyRoles = (roles: Role[]) => {
-    return async (req: RequestWithUser, res: Response, next: NextFunction) => {
-      try {
-        const userRole = req.user.userDocument.role;
+// export class RoleMiddleware {
+//   public verifyRoles = (roles: Role[]) => {
+//     return async (req: RequestWithUser, res: Response, next: NextFunction) => {
+//       try {
+//         const userRole = req.user.userDocument.role;
         
-        if (!roles.includes(userRole)) {
-          throw new HttpException(403, 'You do not have permission to access this resource');
-        }
+//         if (!roles.includes(userRole)) {
+//           throw new HttpException(403, 'You do not have permission to access this resource');
+//         }
         
-        next();
-      } catch (error) {
-        next(error);
-      }
-    };
-  };
-}
+//         next();
+//       } catch (error) {
+//         next(error);
+//       }
+//     };
+//   };
+// }
